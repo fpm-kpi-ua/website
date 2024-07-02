813ac23f704ce7e8f0b5cc69f2671edc354a8bf2
znycheporuk <nycheporuk.zakhar@gmail.com>
2024-07-02 11:38:47 +0300

    fix: format-validation-errors

--- src/shared/format-validation-errors.ts
+++ src/shared/format-validation-errors.ts
@@ -1,11 +1,9 @@
-import type { BaseIssue, BaseSchema, SafeParseResult } from "valibot";
+import type { GenericIssue } from "valibot";
 
-export function formatValidationErrors<
-	T extends BaseSchema<unknown, unknown, BaseIssue<unknown>>,
->(issues: SafeParseResult<T>["issues"]) {
-	return issues!.reduce((acc, issue) => {
+export function formatValidationErrors(issues: GenericIssue[]) {
+	return issues.reduce((acc, issue) => {
 		// @ts-expect-error - issue is not typed
 		acc[issue.path?.at(0)?.key as string] = issue.message;
 		return acc;
-	});
+	}, {});
 }
